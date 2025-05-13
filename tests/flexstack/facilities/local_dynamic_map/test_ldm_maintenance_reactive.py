c8e9b68f4865697449818ee3b3073f801638d6b0
daniel_ulied <daniel.ulied@i2cat.net>
2025-05-13 09:52:39 +0200

    Fixing Add Provider Data

--- tests/flexstack/facilities/local_dynamic_map/test_ldm_maintenance_reactive.py
+++ tests/flexstack/facilities/local_dynamic_map/test_ldm_maintenance_reactive.py
@@ -18,8 +18,8 @@ class TestLDMMaintenanceReactive(unittest.TestCase):
     def test_add_provider_data(self, mock_ldm_maintenace):
         mock_ldm_maintenace().add_provider_data = MagicMock(return_value=1)
         self.ldm_maintenance_reactive.collect_trash = MagicMock()
-        test_dict = MagicMock()
+        test_add_data_provider_req = MagicMock()
 
-        self.ldm_maintenance_reactive.add_provider_data(test_dict)
-        mock_ldm_maintenace().add_provider_data.assert_called_once_with(test_dict)
+        self.ldm_maintenance_reactive.add_provider_data(test_add_data_provider_req)
+        mock_ldm_maintenace().add_provider_data.assert_called_once_with(test_add_data_provider_req)
         self.ldm_maintenance_reactive.collect_trash.assert_called_once()
