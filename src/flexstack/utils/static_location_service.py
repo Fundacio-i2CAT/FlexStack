c8cf2db3d3e50a6234df9491bf2c2dfc02d7b714
mpjordi <jordimarias@gmail.com>
2025-08-12 10:40:48 +0200

    VERSION 0.9.16
    * Ensured UTC timezone
    
    * New version marked
    
    ---------
    
    Co-authored-by: Jordi Marias I Parella <jordi.marias@i2cat.net>

--- src/flexstack/utils/static_location_service.py
+++ src/flexstack/utils/static_location_service.py
@@ -2,8 +2,67 @@ import threading
 import time
 import asyncio
 import datetime
-import json
 from .location_service import LocationService
+
+
+def generate_tpv_dict(current_utc_timestamp: float, latitude: float, longitude: float) -> dict:
+    """
+    Generate a TPV dictionary with the given UTC timestamp, latitude, and longitude.
+
+    Parameters
+    ----------
+    current_utc_timestamp : float
+        The current UTC timestamp.
+    latitude : float
+        The latitude of the location.
+    longitude : float
+        The longitude of the location.
+
+    Returns
+    -------
+    dict
+        A dictionary containing the TPV data.
+    """
+    tt_dt = datetime.datetime.fromtimestamp(
+        current_utc_timestamp, datetime.timezone.utc)
+    tt = tt_dt.isoformat()
+    tt = tt[:-9] + "Z"
+    return {'class': 'TPV',
+            'device': '/dev/ttyACM0',
+            'mode': 3,
+            'time': tt,
+            'ept': 0.005,
+            'lat': latitude,
+            'lon': longitude,
+            'alt': 163.8,
+            'epx': 10.0,
+            'epy': 10.0,
+            'epv': 10.0,
+            'track': 0.0,
+            'speed': 0.0,
+            'climb': 0.0,
+            'eps': 0.0}
+
+
+def generate_tpv_dict_with_current_timestamp(latitude: float, longitude: float) -> dict:
+    """
+    Generate a TPV dictionary with the current UTC timestamp, latitude, and longitude.
+
+    Parameters
+    ----------
+    latitude : float
+        The latitude of the location.
+    longitude : float
+        The longitude of the location.
+
+    Returns
+    -------
+    dict
+        A dictionary containing the TPV data.
+    """
+    current_utc_timestamp = datetime.datetime.now(
+        datetime.timezone.utc).timestamp()
+    return generate_tpv_dict(current_utc_timestamp, latitude, longitude)
 
 
 class AsyncStaticLocationService(LocationService):
@@ -44,23 +103,8 @@ class AsyncStaticLocationService(LocationService):
         Start the Static Location Service.
         """
         while True:
-            tt = datetime.datetime.fromtimestamp(time.time()).strftime(
-                "%Y-%m-%dT%H:%M:%S.%f"
-            )
-            tt = tt[:-3]
-            tt = tt + "Z"
-            tt = tt.encode("utf-8")
-            tpv_string = (
-                b'{"class":"TPV","device":"/dev/ttyACM0","mode":3,"time":"'
-                + tt
-                + b'","ept":0.005,"lat":'
-                + str(self.latitude).encode("utf-8")
-                + b',"lon":'
-                + str(self.longitude).encode("utf-8")
-                + b',"alt":163.800,"epx":10.000,"epy":10.000,"epv":10.000,'
-                + b'"track":0.0000,"speed":0.000,"climb":0.000,"eps":0.00}'
-            )
-            json_tpv = json.loads(tpv_string)
+            json_tpv = generate_tpv_dict_with_current_timestamp(
+                self.latitude, self.longitude)
             self.send_to_callbacks(json_tpv)
             await asyncio.sleep(self.period / 1000)
 
@@ -100,7 +144,8 @@ class ThreadStaticLocationService(LocationService):
         self.period = period
         self.latitude = latitude
         self.longitude = longitude
-        self.location_service_thread = threading.Thread(target=self.start, daemon=True)
+        self.location_service_thread = threading.Thread(
+            target=self.start, daemon=True)
         self.stop_event = threading.Event()
         self.location_service_thread.start()
 
@@ -110,22 +155,7 @@ class ThreadStaticLocationService(LocationService):
         Start the Static Location Service.
         """
         while not self.stop_event.is_set():
-            tt = datetime.datetime.fromtimestamp(time.time()).strftime(
-                "%Y-%m-%dT%H:%M:%S.%f"
-            )
-            tt = tt[:-3]
-            tt = tt + "Z"
-            tt = tt.encode("utf-8")
-            tpv_string = (
-                b'{"class":"TPV","device":"/dev/ttyACM0","mode":3,"time":"'
-                + tt
-                + b'","ept":0.005,"lat":'
-                + str(self.latitude).encode("utf-8")
-                + b',"lon":'
-                + str(self.longitude).encode("utf-8")
-                + b',"alt":163.800,"epx":10.000,"epy":10.000,"epv":10.000,"track":0.0000,"speed":0.000,'
-                + b'"climb":0.000,"eps":0.00}'
-            )
-            json_tpv = json.loads(tpv_string)
+            json_tpv = generate_tpv_dict_with_current_timestamp(
+                self.latitude, self.longitude)
             self.send_to_callbacks(json_tpv)
             time.sleep(self.period / 1000)
